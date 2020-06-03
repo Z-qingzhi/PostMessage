87c2968cff5acc9293f3d7cada97014adcef0105
Z-qingzhi <1105310910@qq.com>
2020-06-03 14:37:22 +0800

    first commit

---
+++ express1/README.md
@@ -0,0 +1,9 @@
+## 安装依赖
+```
+npm install
+```
+
+## 启动
+```
+npm install
+```
---
+++ express1/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var cookieParser = require("cookie-parser");
+
+
+app.use(express.static(path.join(__dirname, 'views')))
+app.use(cookieParser());
+
+
+
+
+app.listen(3000, () => {
+  console.log(`App listening at port 3000`)
+})
